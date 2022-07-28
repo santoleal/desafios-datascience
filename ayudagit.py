8e92f491ff46c76796888202529778c496e8a8ab
FelipeLealArancibia <felipeleal@contesys.cl>
2022-07-28 00:30:58 -0400

    Antes del envío a github. Todavía probando.

--- ayudagit.py
+++ ayudagit.py
@@ -3,4 +3,8 @@
 2. Git status
 3. Git add <filename>
 4. Git ignore      . Es para omitir archivos en el rastreo GIT
+5. Git branch      . Muestra las ramas de los proyectos.
+6. Git branch <nombre nueva rama alternativa>   . Crea una copia para poder trabajare sin afectar el master
+7. Git checkout <nombre de nueva rama>    . Se cambia la rama de trabajo para poder meter mano tranquilamente.
+8.  
 """
