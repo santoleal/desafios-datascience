67aa864db63c4e4f41df1a8b8501959d48ff60c4
FelipeLealArancibia <felipeleal@contesys.cl>
2022-07-27 23:33:06 -0400

    he agregado un git ignore

--- ayudagit.py
+++ ayudagit.py
@@ -2,5 +2,5 @@
 1. Acceso / Inicio GIT: git init   . Se crea una carpeta oculta. Ya se estrá trabajando en el Working Directory
 2. Git status
 3. Git add <filename>
-
+4. Git ignore      . Es para omitir archivos en el rastreo GIT
 """
